8f35dc03ed945c5514a6749b3b52fa69e83cba4e
Ahany42 <ahanyfathy42@gmail.com>
2024-03-03 17:05:54 +0200

    Retrieving Actors id

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+       
+            string ordb = "Data source=orcl;User Id=scott;Password=tiger;";
+            OracleConnection conn;
+           
+            
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select ActorID from Actors";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0]);
+            }
+            dr.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Dispose();
         }
     }
 }
--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Oracle.DataAccess.Client;
-using Oracle.DataAccess.Types;
+
 namespace WindowsFormsApp
 {
+   
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
